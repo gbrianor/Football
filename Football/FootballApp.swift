e5e12b248e27688f0e3107eb7f639f36dde581b4
Giovanna Briano <gbrianor@gmail.com>
2025-06-20 15:16:16 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Giovanna Briano on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballTests/FootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by Giovanna Briano on 6/20/25.
+//
+
+import Testing
+@testable import Football
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
